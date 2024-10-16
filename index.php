d1909755d3b40c38be9a47693704aeb79555eee2
Sandeepchouhan11 <sandeepchouhan8349@gmail.com>
2024-10-16 12:15:42 +0530

    my first php

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Student Ragistration</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div style="margin-top: -20px;" class="form">
+<form action="">
+  <div class="headerdiv"> Applicant Details</div>
+
+  <div class="maincontainer">
+
+    <div class='mainCell'> Student Name  <span class="startcolor">*</span></div>
+    <div class='inputCell'><input type="text"></div>
+    <div class='mainCell'>Fathar Name  <span class="startcolor">*</span></div>
+    <div class='inputCell'><input type="text"></div>
+
+  </div>
+
+  <div class="maincontainer">
+
+    <div class='mainCell'>Mother Name</div>
+    <div class='inputCell'><input type="text"></div>
+    <div class='mainCell'>Gender  <span class="startcolor">*</span></div>
+    <div class='genderinput'><input class="inputcell" type="radio" name="gender">Male <input type="radio" name="gender">Female</div>
+
+ 
+  </div>
+
+<div class="maincontainer">
+
+  <div class='mainCell'>Mobile No.</div>
+  <div class='inputCell'><input type="text"></div>
+  <div class='mainCell'>Email  <span class="startcolor">*</span></div>
+  <div class='inputCell'><input type="text"></div>
+
+</div>
+
+<div class="maincontainer">
+
+  <div class='mainCell'>Category  <span class="startcolor">*</span></div>
+  <div class='castCell'>
+    <select name="" id="">
+      <option>SELECT CASTE</opttion>
+      <option>GENRAL</opttion>
+      <option>OBC</opttion>
+      <option>SC</opttion>
+      <option>SC</option>
+    </select></div>
+  <div class='mainCell'>Date Of Birth  <span class="startcolor">*</span></div>
+  <div class='inputCell'><input type="date"></div>
+
+
+</div>
+
+<div class="divcontainer">
+
+  <div class='mainCell'>Current Address  <span class="startcolor">*</span></div>
+  <div class='adressinput'><input type="text"></div>
+  
+</div>
+<div class="adressheader">Same Address to click the checkbox <input type="checkbox"></div>
+
+<div class="maincontainer">
+
+  <div class='mainCell'>Parmanant Address  <span class="startcolor">*</span></div>
+  <div class='adressinput'><input type="text"></div>
+  
+</div>
+<div class="divcontainer">
+
+  <div class='mainCell'> Caste Certificate  <span class="startcolor">*</span> </div>
+  <div class='genderinput'><input class="inputcell" type="radio" name="caste">YES <input type="radio" name="caste">NO</div>
+
+  <div class='mainCell'>Income Certificate</div>
+  <div class='genderinput'><input class="inputcell" type="radio" name="Income_Certificate">YES <input type="radio" name="Income_Certificate">NO</div>
+
+</div>
+
+<div class="maincontainer">
+
+  <div class='mainCell'>Yearly Income  </div>
+  <div class='inputCell'><input type="text"></div>
+  <div class='mainCell'>MP Domecile </div>
+  <div class='genderinput'><input class="inputcell" type="radio" name="Domecile">YES <input type="radio" name="Domecile">NO</div>
+
+
+</div>
+
+  <div class="maincontainer">
+
+  <div class='mainCell'> Aadhar Number  <span class="startcolor">*</span></div>
+  <div class='inputCell'><input type="text"></div>
+  <div class='mainCell'>Uploade Aadhar file  <span class="startcolor">*</span></div>
+  <div class='inputCell'><input type="file"></div>
+
+</div>
+
+
+</div>
+
+<div class="form">
+<div class="headerdiv"> Education Deatails</div>
+
+<div class="maincontainer">
+
+  <div class='mainCell'> Enrollment No.</div>
+  <div class='inputCell'><input type="text"></div>
+  <div class='mainCell'>Date of Enrollment</div>
+  <div class='inputCell'><input type="text"></div>
+
+</div>
+
+<div class="maincontainer">
+
+  <div class='mainCell'> Class</div>
+  <div class='inputCell'><input type="text"></div>
+  <div class='mainCell'>Admission Year</div>
+  <div class='inputCell'><input type="text"></div>
+
+</div>
+
+
+
+
+</div>
+
+<div class="form">
+
+<div class="headerdiv smalheader" > Class X  <span class="startcolor">*</span></div>
+
+
+
+<div class="maincontainer">
+
+  <div class='mainCell'> Rall No. </div>
+  <div class='inputCell'><input type="text" placeholder="Roll Number"></div>
+  <div class='mainCell'>Passing Year</div>
+  <div class='inputCell'><input type="text" placeholder="Year"></div>
+  <div class='mainCell'>Board Name</div>
+  <div class='inputCell'><input type="text" placeholder="Board Name"></div>
+
+</div>
+<div class="maincontainer">
+
+  <div class='mainCell'> Upload X Marksheet </div>
+  <div class='inputCell'><input type="File" ></div>
+  <div class='mainCell'> Grade</div>
+  <div class='inputCell'><input type="text" placeholder="Enter Your grade"></div>
+  <div class='mainCell'>Percent</div>
+  <div class='inputCell'><input type="text" placeholder="Percent"></div>
+
+</div>
+</div>
+<div class="form">
+
+<div class="headerdiv smalheader" > Class XII  <span class="startcolor">*</span></div>
+
+
+<div class="maincontainer">
+
+  <div class='mainCell'> Rall No. </div>
+  <div class='inputCell'><input type="text" placeholder="Roll Number"></div>
+  <div class='mainCell'>Passing Year</div>
+  <div class='inputCell'><input type="text" placeholder="Year"></div>
+  <div class='mainCell'>Board Name</div>
+  <div class='inputCell'><input type="text" placeholder="Board Name"></div>
+
+</div>
+<div class="maincontainer">
+
+  <div class='mainCell'> Upload XII Marksheet </div>
+  <div class='inputCell'><input type="File" ></div>
+  <div class='mainCell'> Grade</div>
+  <div class='inputCell'><input type="text" placeholder="Enter Your grade"></div>
+  <div class='mainCell'>Percent</div>
+  <div class='inputCell'><input type="text" placeholder="Percent"></div>
+
+</div>
+
+</div>
+
+
+
+<div class="form">
+
+<div class="headerdiv smalheader" > Graduation Deatails <span class="startcolor">*</span></div>
+
+
+
+<div class="maincontainer">
+
+  <div class='mainCell'> Enrollment No. </div>
+  <div class='inputCell'><input type="text" placeholder="Enrollment No."></div>
+  <div class='mainCell'>Passing Year</div>
+  <div class='inputCell'><input type="text" placeholder="Year"></div>
+  <div class='mainCell'>Univarcity Name</div>
+  <div class='inputCell'><input type="text" placeholder="Univarcity Name"></div>
+
+</div>
+<div class="maincontainer">
+
+  <div class='mainCell'> Upload Final year Marksheet </div>
+  <div class='inputCell'><input type="File" ></div>
+  <div class='mainCell'> Grade</div>
+  <div class='inputCell'><input type="text" placeholder="Enter Your grade"></div>
+  <div class='mainCell'>Percent</div>
+  <div class='inputCell'><input type="text" placeholder="Percent"></div>
+
+</div>
+
+</div>
+
+</form>
+</div>
+
+
+
+</body>
+</html>
